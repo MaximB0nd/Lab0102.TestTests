67e876c94e368392332f4b1d0d535fd85d924549
MaximB0nd <sersim11@yandex.ru>
2025-04-18 12:33:56 +0300

    Init

---
+++ EmployeeTests/CheckNameTest.cs
@@ -0,0 +1,40 @@
+﻿
+using NUnit.Framework;
+using EmployeeLibrary;
+
+namespace EmployeeTests
+{
+    [TestFixture]
+    public class CheckNameTest
+    {
+        [Test]
+        public void Test1()
+        {
+           
+        }
+        
+        [Test]
+        public void Test2()
+        {
+            
+        }
+        
+        [Test]
+        public void Test3()
+        {
+            
+        }
+        
+        [Test]
+        public void Test4()
+        {
+            
+        }
+        
+        [Test]
+        public void Test5()
+        {
+            
+        }
+    }
+}
