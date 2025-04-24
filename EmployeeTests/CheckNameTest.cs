aca35013d146cbff57d3d5371b873105596cf8ab
WinParal <sersim11@yandex.ru>
2025-04-24 12:27:42 +0300

    CheckNameTest

--- EmployeeTests/CheckNameTest.cs
+++ EmployeeTests/CheckNameTest.cs
@@ -1,6 +1,8 @@
 ﻿
+using System;
 using NUnit.Framework;
 using EmployeeLibrary;
+using static EmployeeLibrary.Employee;
 
 namespace EmployeeTests
 {
@@ -10,31 +12,82 @@ namespace EmployeeTests
         [Test]
         public void Test1()
         {
-           
+            var employee = new Employee("A1",
+                new DateTime(1990, 1, 1),
+                Gender.MALE, 1000,
+                CurrentPosition.SENIOR, Education.HIGHER);
+            Assert.IsTrue(employee.checkName());
+
         }
-        
+
         [Test]
         public void Test2()
         {
-            
+            var employee = new Employee("1A",
+                new DateTime(1990, 1, 1),
+                Gender.MALE, 1000,
+                CurrentPosition.SENIOR, Education.HIGHER);
+            Assert.IsFalse(employee.checkName());
         }
         
         [Test]
         public void Test3()
         {
-            
+            var employee = new Employee("A1B2",
+                new DateTime(1990, 1, 1),
+                Gender.MALE, 1000,
+                CurrentPosition.SENIOR, Education.HIGHER);
+            Assert.IsFalse(employee.checkName());
         }
         
         [Test]
         public void Test4()
         {
-            
+            var employee = new Employee("A@B",
+                new DateTime(1990, 1, 1),
+                Gender.MALE, 1000,
+                CurrentPosition.SENIOR, Education.HIGHER);
+            Assert.IsFalse(employee.checkName());
         }
         
         [Test]
         public void Test5()
         {
-            
+            var employee = new Employee("A1B2",
+                new DateTime(1990, 1, 1),
+                Gender.MALE, 1000,
+                CurrentPosition.SENIOR, Education.HIGHER);
+            Assert.IsFalse(employee.checkName());
+        }
+        
+        [Test]
+        public void Test6()
+        {
+            var employee = new Employee("A",
+                new DateTime(1990, 1, 1),
+                Gender.MALE, 1000,
+                CurrentPosition.SENIOR, Education.HIGHER);
+            Assert.IsFalse(employee.checkName());
+        }
+        
+        [Test]
+        public void Test7()
+        {
+            var employee = new Employee("ABCDEFGHIJKLMNOP",
+                new DateTime(1990, 1, 1),
+                Gender.MALE, 1000,
+                CurrentPosition.SENIOR, Education.HIGHER);
+            Assert.IsFalse(employee.checkName());
+        }
+        
+        [Test]
+        public void Test8()
+        {
+            var employee = new Employee("Иван1",
+                new DateTime(1990, 1, 1),
+                Gender.MALE, 1000,
+                CurrentPosition.SENIOR, Education.HIGHER);
+            Assert.IsTrue(employee.checkName());
         }
     }
 }
